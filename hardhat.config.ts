53cbe891c7f719ec3e730bd027b9d095fe87d3ca
Nathan Thomas <nwthomas@me.com>
2022-04-23 23:21:58 -0700

    Update version of Solidity

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,12 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
 
 dotenv.config();
 
@@ -23,7 +24,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.6",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
